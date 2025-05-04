abfbb6cd389da0372d38eb98573b782043cee4d3
Simon Gravelle <simon.gravelle@live.fr>
2025-05-04 21:20:09 +0200

    added analysis script

--- analysis/varying-dumping/analysis.py
+++ analysis/varying-dumping/analysis.py
@@ -8,14 +8,14 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 
 from utilities import save_result, get_git_repo_path
 
-def process(d, git_path):
+def process(n, git_path):
     """Process one temperature point"""
-    data_dir = os.path.join(git_path, "data", "varying-dumping", f"d{d}")
+    data_dir = os.path.join(git_path, "data", "varying-n-particle", f"n{n}")
     topology_file = os.path.join(data_dir, "topology.data")
     trajectory_file = os.path.join(data_dir, "dump.xtc")
 
     if not os.path.exists(topology_file) or not os.path.exists(trajectory_file):
-        print(f"[d={d}] Missing MD data")
+        print(f"[n={n}] Missing MD data")
         return
 
     try:
@@ -28,18 +28,19 @@ def process(d, git_path):
             number_i=1)
         nmr.run_analysis()
         
-        save_result(nmr, name=f"d{d}")
-        print(f"d={d} Success")
+        save_result(nmr, name=f"n{n}")
+        print(f"n={n} Success")
     except Exception as e:
-        print(f"[d={d}] Error: {e}")
+        print(f"[n={n}] Error: {e}")
 
 def main(max_iterations):
     git_path = get_git_repo_path()
-    dumping = ["1", "2", "3", "4", "7", "10", "15", "22", "33", "49"]
+    n_parts = ["200", "326", "532", "868", "1417", "2312", "3772", "6154", "10042", "16383"]
+
     for iteration in range(max_iterations):
         print(f"\n--- Iteration {iteration + 1} ---")
         with ProcessPoolExecutor() as executor:
-            futures = [executor.submit(process, d, git_path) for d in dumping]
+            futures = [executor.submit(process, n, git_path) for n in n_parts]
             for future in as_completed(futures):
                 future.result()
 
